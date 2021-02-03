4d9b80051aa17f45473f651f2730d66c60da7d7e
Andrew Wanamaker <agwanamaker@protonmail.ch>
2021-02-03 12:51:55 -0600

    Added  helloworldtest folder with the basic hello world test inside

--- helloWorldTest/HWTran/HWTran.ino
+++ helloWorldTest/HWTran/HWTran.ino
@@ -1,24 +1,24 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-// Radio settings
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-
 void setup() {
-  // Radio Set Up
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
 void loop() {
-  // Send time value
   const char text[] = "Hello World";
   radio.write(&text, sizeof(text));
   delay(1000);
-  /*int time = 5;
-  radio.write(&time, sizeof(time));
-  delay(1000); */
 }
