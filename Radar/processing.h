9efebf677946219e2c293112609274c0b8d54489
Short <UM-AD\snspzv@rc11xcs213.managed.mst.edu>
2021-02-17 23:49:56 -0600

    new speed calc

--- Radar/processing.h
+++ Radar/processing.h
@@ -19,9 +19,15 @@ double getPeakFreq(uint16_t & rawMaxVal, uint8_t filled)
   return(FFT.MajorPeak(data_in[filled], SAMPLE_MAX, SAMPLING_FREQUENCY, rawMaxVal));
 }
 
+
+//Uses Doppler equation:
+  //Fd = 2 * v * cos(theta) *(Ft / c)
+//Uses conversion rate of:
+  //1 m/s = 2.236936 mph
+//WILL NEED TO ACCOUNT FOR THETA
 double freqToMPH(double freq)
 {
-  return (freq * double(0.013879)); 
+  return (freq * double(0.013557)); 
 }
 
 double findSpeed(uint8_t filled)
@@ -31,7 +37,7 @@ double findSpeed(uint8_t filled)
 
   //check if amplitude at peak frequency is below movement threshold
   //probably will need to adjust this value later
-  if(maxVal < 10)
+  if(maxVal < 20)
   {
     return 0;
   }
