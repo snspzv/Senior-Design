36d1ac0d00f1ac8c340d2b26df8350ebf9aaaeb7
Andrew Wanamaker <agwanamaker@protonmail.ch>
2021-02-22 14:06:29 -0600

    Added comments and RELAY var

--- Signal/light.h
+++ Signal/light.h
@@ -1,28 +1,33 @@
 #ifndef light_h
 #define light_h
 
-void lightOnSolid()
-{
-  digitalWrite(4, HIGH);
-}
+// Change control pin by changing value of RELAY
+const int RELAY = 4;
 
-void lightOnBlinking()
-{
-    digitalWrite(4, HIGH);
-    delay(1000);
-    digitalWrite(4, LOW);
-    delay(1000);
-}
-
-void lightOff()
-{
-  digitalWrite(4, LOW);
-}
-
+// Setup funtion for relay control
 void lightInit()
 {
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
+  pinMode(RELAY, OUTPUT); // Set pin mode
+  digitalWrite(RELAY, LOW); // making sure that it is off
+}
+
+void lightOnSolid() // Turns relay on
+{
+  digitalWrite(RELAY, HIGH); // Push 5 V to Pin 4
+}
+
+void lightOnBlinking() // Blink relay
+{
+  // Light blinks for one second
+  digitalWrite(RELAY, HIGH);
+  delay(1000);
+  digitalWrite(RELAY, LOW);
+  delay(1000);
+}
+
+void lightOff() // Turns relay off
+{
+  digitalWrite(RELAY, LOW); // Push 0 V to Pin 4
 }
 
 #endif /* light_h */
