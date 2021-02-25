5d33c85b3b65977984e743f55c68a6a779b6e7ab
Levi Vieth <lav7v3@mst.edu>
2021-02-24 23:20:11 -0600

    radioTimer ready for testing? light timer needs attention

--- Signal/lightTimer.h
+++ Signal/lightTimer.h
@@ -3,46 +3,51 @@
 
 #include "constants.h"
 #include "light.h"
-static const uint32_t AVAILABLE_TICKS = 65535; //@prescale 1024
+static const uint32_t AVAILABLE_TICKS = 255; //@prescale 1024
 static const uint32_t TICKS_IN_SECOND = 15625; //@prescale 1024
 extern volatile uint8_t g_state;
 static volatile uint8_t timer_iterations;
 
+// FIX TO RUN ON TIMER 2 OR 0
 void lightTimerInit()
 {
-  TCCR1A = 0;
-  TCCR1B = (1 << WGM12);   //CTC mode (compare OCR1A)
+  TCCR2A = 0;
+  TCCR2B = (1 << WGM22);   //CTC mode (compare OCR1A)
 }
-
+// has to be transmitted as a double, thus casting 
 void startLightTimer(double seconds)
 { 
   uint32_t ticks = uint32_t(seconds * TICKS_IN_SECOND);
   timer_iterations = ((ticks / AVAILABLE_TICKS) + 1);
-  uint16_t one_iteration = ticks / (uint32_t)timer_iterations;
-  TCCR1B = (1 << WGM12);
-  OCR1AH = uint8_t(one_iteration >> 8);
-  OCR1AL = uint8_t(one_iteration & 0x00FF);
-  TIMSK1 = (1 << OCIE1A); //Interrupts enabled on OCR1 match
-  TCCR1B |= (1 << CS12) | (1<< CS10); //1024 prescaler -> 15.625 kHz (actually starts timer)
+  uint8_t one_iteration = ticks / (uint32_t)timer_iterations;
+  TCCR2B = (1 << WGM22);
+  OCR2A = uint8_t(one_iteration);
+  TIMSK2 = (1 << OCIE2A); //Interrupts enabled on OCR1 match
+  TCCR2B |= ((1 << CS22) | (1<<CS21) | (1<< CS20)); //1024 prescaler -> 15.625 kHz (actually starts timer)
 }
 
 void restartLightTimer(double seconds)
 {
-  TCCR1B &= ~((1 << CS12) | (1<< CS10)); //Stop timer to restart w/ new values
+  TCCR2B &= ~((1 << CS22) | (1<<CS21) | (1<< CS20)); //Stop timer to restart w/ new values
   startLightTimer(seconds);
 }
 
+//is startlightblinking just blinking for a second on and off?
+//run into the issue with ocr2, not sure how to fix at this moment
+/*
 void startLightBlinking()
 {
-  TIMSK1 = 0; //Disable timer 1 interrupts
-  TCCR1A |= (1 << COM1A0); //Toggle OC1A (LED pin) on compare match
+  TIMSK2 = 0; //Disable timer 1 interrupts
+  TCCR2A |= (1 << COM1A0); //Toggle OC1A (LED pin) on compare match
   OCR1AH = uint8_t(TICKS_IN_SECOND >> 8);
   OCR1AL = uint8_t(TICKS_IN_SECOND & 0x00FF);
-  TCCR1B |= (1 << CS12) | (1 << CS10); //1024 prescale, actually starts timer
+  //***TODO check prescale for either timer2 or timer0 
+  TCCR2B |= ((1 << CS22) | (1<<CS21) | (1<< CS20)); //1024 prescale, actually starts timer
 }
+*/
 
 //Used to shutoff timer after done
-ISR(TIMER1_COMPA_vect)
+ISR(TIMER2_COMPA_vect)
 {
   uint8_t temp = SREG;
   timer_iterations--;
@@ -50,7 +55,7 @@ ISR(TIMER1_COMPA_vect)
   {
     g_state = LIGHT_OFF;
     lightOff();
-    TCCR1B &= ~((1 << CS12) | (1<< CS10));
+    TCCR1B &= ~((1 << CS22) | (1<<CS21) | (1<< CS20));
   }
   SREG = temp;
 }
--- Signal/radioTimer.h
+++ Signal/radioTimer.h
@@ -1,33 +1,55 @@
-//#ifndef radiotimer_h
-//#define radiotimer_h
+#ifndef radiotimer_h
+#define radiotimer_h
 //
 //#include "lightTimer.h"
 //extern volatile uint8_t g_state;
 //
-//void initRadioTimer()
-//{
-//  
-//}
-//
-//void startRadioTimer()
-//{
-//  
-//}
-//
-//void stopRadioTimer()
-//{
-//  
-//}
-//
-//void restartRadioTimer()
-//{
-//  
-//}
-//
-//ISR(/*timer interrupt vector*/)
-//{
-//  startLightBlinking();
-//  g_state = LIGHT_BLINKING;
-//  stopRadioTimer();
-//}
-//#endif /* radiotimer_h */
+/*
+ * static const uint8_t OCR_MAX = 65535; 
+   static const uint16_t TICKS_IN_SECOND = 15625; //@prescale 1024
+  
+   //time is set in 500 milliseconds ASSUMPTION IS THAT WE WILL NOT GO ABOVE 999 ms 
+   static const uint16_t TIME = ((500 * TICKS_IN_SECOND)/ (uint16_t)1000);
+ * 
+ */
+
+/*
+ 
+
+ //sets timer 1 to be in ctc, with cycles needed equating to 500 ms in ocr
+void initRadioTimer()
+{
+  
+  TCCR1A = 0x00;
+  TCCR1B = (1<<WGM12); //CTC mode
+
+  //upper bits pushed in high
+  OCR1AH = (TIME>>8);
+  //get lower 8 bits into Low
+  OCR1AL = (TIME & 0x00FF);
+  
+}
+*/
+/*
+ //just use a const and no param, just set prescale ->101
+void startRadioTimer()
+{
+    TCCR1B |= ((1<<CS12)| (1<<CS20)); // set prescale to 1024
+}
+
+/*
+void stopRadioTimer()
+{
+  TCCR1B |= ((0<<CS12)| (0<<CS10));
+  or TCCR1B &= ~((1<<CS12)|(1<<CS10));
+}
+*/
+/*
+ISR(/*timer interrupt vector)
+{
+  startLightBlinking();
+  g_state = LIGHT_BLINKING;
+  stopRadioTimer();
+}
+*/
+#endif /* radiotimer_h */
