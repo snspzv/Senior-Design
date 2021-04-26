acd15d46a1024f8f1b049eb3a3f3edadbc1d270f
sam <snspzv@mst.edu>
2021-04-26 15:03:51 -0500

    radio name fix

---
+++ Radar/Radar.ino
@@ -0,0 +1,40 @@
+#include "arduinoFFT.h"
+#include "analogConversion.h"
+#include "processing.h"
+#include "radio.h"
+#include <ADC.h>
+#include <ADC_util.h>
+
+volatile uint8_t buffer_status = 0;
+const uint8_t BOTH_READY = 3;
+const uint8_t TOWARDS = 0;
+const uint8_t AWAY = 1;
+const uint8_t NO_MOVEMENT = 2;
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+  ADCInit();
+  radioInit();
+}
+
+void loop() {
+  if(buffer_status == BOTH_READY)
+  { 
+    buffer_status = 0;
+    uint8_t dir = getDirection();
+
+    if((dir == NO_MOVEMENT) || (dir == AWAY))
+    {
+      transmit(0);
+    }
+
+    else
+    {
+      uint32_t ti = lightOnTime();
+      Serial.println(ti);
+      //transmit(lightOnTime());
+    }
+  }
+}
